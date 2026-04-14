69448c5522faef7470508d343578285fd90bd605
aidyn <a.nurlan@documentolog.com>
2026-04-14 17:14:28 +0500

    deleting: some troubles with project

--- app/db/init_db.py
+++
@@ -1,9 +0,0 @@
-from app.db.base import Base
-from app.db.session import engine
-from app.models.audit_log import AuditLog
-from app.models.refresh_token import RefreshToken
-from app.models.user import User
-
-
-def init_db() -> None:
-    Base.metadata.create_all(bind=engine)
